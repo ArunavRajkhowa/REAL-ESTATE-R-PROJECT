f3b7d6463764a5eed9213b88a2c0d55627244497
ArunavRajkhowa <arunav.rajkhowa2@gmail.com>
2022-10-07 14:25:29 +0530

    Debugged code and built linear regression model

--- Real_Estate.R
+++ Real_Estate.R
@@ -113,17 +113,19 @@ a1=ifelse(train$Type =='h',mean(train$Price,na.rm=T),ifelse())
   vis_dat(df)
 
   
-# Data Preparation
-  df = df %>% select(-Address) #dropping address
+# Data Preparation--------------------------------------
+  #df = df %>% select(-Address) #dropping address
   df$Price=as.numeric(df$Price) #treating response  column separately
-
+  df$Postcode=as.factor(df$Postcode) #treating postcode as category
     dp_pipe=recipe(Price~.,data=df) %>%
- 
+      
+    update_role(Address,Postcode,new_role = "drop_vars") %>%
     update_role(Suburb,Type,Method,SellerG,
-                CouncilArea,new_role="to_dummies") %>% 
-
+                CouncilArea ,new_role="to_dummies") %>% 
+    
+    step_rm(has_role("drop_vars")) %>%
     step_unknown(has_role("to_dummies"),new_level="__missing__") %>% 
-    step_other(has_role("to_dummies"),threshold =0.02,other="__other__") %>% 
+    step_other(has_role("to_dummies"),threshold =0.03,other="__other__") %>% 
     step_dummy(has_role("to_dummies")) %>%
     step_impute_median(all_numeric(),-all_outcomes())
   
@@ -138,11 +140,49 @@ vis_dat(prep_df) #all looks good!
 prep_df=prep_df[!((is.na(prep_df$Price)) & prep_df$data=='train'), ]
 
 
-#separating train and test dataset
-train=prep_df %>% filter(data=='train') %>% select(-data)
-test=prep_df %>% filter(data=='test') %>% select(-data,-Price)
+#separating train and test dataset---------------------------
+train=prep_df %>% filter(data=='train') %>% select(-data,-Type_X__other__)
+test=prep_df %>% filter(data=='test') %>% select(-data,-Price,-Type_X__other__)
+vis_dat(train)
 vis_dat(test)
-# Data Prep ends here
+# Data Prep ends here , subtracting typeXother because it contained only 0s
 
 
+#MODEL  building starts here-----------------------------------------
+
+#1st model : simple linear regression------------------------
+
+
+for_vif=lm(Price~.,data=train)
+
+
+sort(vif(for_vif),decreasing = T)[1:3]
+
+rm(for_vif)
+fit=lm(Price~.,data=train)
+fit=stats::step(fit)
+summary(fit)
+
+fit=lm(formula = Price ~ Rooms + Distance + Bedroom2 + Bathroom + 
+         Car + Landsize + BuildingArea + YearBuilt + Suburb_X__other__ + 
+         Type_t + Type_u + Method_S + 
+         SellerG_Buxton + SellerG_Jellis + SellerG_Marshall + SellerG_X__other__ + 
+         CouncilArea_Banyule + CouncilArea_Bayside + CouncilArea_Boroondara + 
+         CouncilArea_Darebin + CouncilArea_Hobsons.Bay + CouncilArea_Maribyrnong + 
+         CouncilArea_Melbourne + CouncilArea_Moonee.Valley + CouncilArea_Moreland + 
+         CouncilArea_Port.Phillip + CouncilArea_Yarra + 
+         CouncilArea_X__other__, data = train)
+
+
+#PREDICTION
+test.predictions=predict(fit,newdata=test)
+
+write.csv(test.predictions,'SimpleLinearRegression.csv',row.names = F)
+plot(fit,1) # residual vs fitted values => non-linearity in the data exists or not
+
+plot(fit,2) # errors are normal or not
+
+plot(fit,3) # variance is constant or not
+
+plot(fit,4) # outliers in the data if cook's distance >1
 
